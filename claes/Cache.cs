d7c5f5da049f3cdd5bf3a86d1c2ae77e5f835e9f
matanki.saito <matanki.saito@gmail.com>
2019-05-06 13:36:42 +0900

    #7

---
+++ claes/Cache.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace claes
+{
+    public class Cache
+    {
+        public string pack_md5 { get; set; }
+        public List<FileInfo> file_list { get; set; }
+    }
+
+    public class FileInfo
+    {
+        public string path { get; set; }
+        public string md5 { get; set; }
+        public DateTime last_update { get; set; }
+    }
+}
